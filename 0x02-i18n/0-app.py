97fbcb3000c0bfffc0400ac460ce845ac27cb774
Anthony Gbobo <psalmseins@gmail.com>
2024-08-02 16:22:45 -0700

    Flast app setup

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Basic flask application setup
+"""
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Rendering a basic html template '0-index.html'
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
